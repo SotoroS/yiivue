942edbc3b77d3a85a5e40170a2d2c6eb46114db9
pershmaj <averingd@gmail.com>
2019-03-14 22:51:11 +0400

    Поправлен axios

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
 
-export const http = axios.create({
-    baseURL: 'http://localhost:1228'
-    // baseURL: ''   //prodaction url
-})
+window.axios = require('axios');
 
-http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    http.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found');
 }

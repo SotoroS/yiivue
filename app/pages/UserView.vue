1480c2dab3050fa370cff3cf0e908f01c15ce4c0
pershmaj <dmitry@averin.pro>
2019-03-16 10:06:26 +0300

    vue struct

---
+++ app/pages/UserView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="user-view" class="user-view">
+        User view
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'user-view',
+    }
+</script>
+
+<style>
+
+</style>

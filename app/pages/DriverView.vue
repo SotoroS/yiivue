9c54799c90a7e19340cf282f8cc6588dcda4bb3c
SparkleDiscord <j.gribanoff@yandex.ru>
2019-03-16 14:27:23 +0400

    Driver map

--- app/pages/DriverView.vue
+++ app/pages/DriverView.vue
@@ -1,12 +1,45 @@
 <template>
     <div id="driver-view" class="driver-view">
-        Driver view
+        <div id="driver-header">
+            <p>{driverFullName}</p>
+            <p>{driverRoute}</p>
+        </div>
+        <div id="driver-clock">
+            <p>{routeTime}</p>
+        </div>
+        <div id="map-filter">
+            <here-map></here-map>
+        </div>
+        <div id="drive-button">
+            <p>{buttonText}</p>
+        </div>
     </div>
 </template>
 
 <script>
+    import HereMap from '../components/HereMap'
+
     export default {
         name: 'driver-view',
+        components: {'here-map': HereMap},
+        data(){
+            return {
+                driverFirstName: "",
+                driverSecondName: "",
+                driverRoute: "",
+                buttonText: "",
+                routeTimeMinutes: 0,
+                routeTimeSeconds: 0
+            }
+        },
+        computed: {
+            driverFullName() {
+                return this.driverFirstName + " " + this.driverSecondName;
+            },
+            routeTime() {
+                return this.routeTimeMinutes + ":" + this.routeTimeSeconds;
+            }
+        }
     }
 </script>
 

9cbef321af650524c51ec68d0154a32cc1246792
SparkleDiscord <j.gribanoff@yandex.ru>
2019-03-16 16:38:59 +0400

    fix

--- app/pages/DriverView.vue
+++ app/pages/DriverView.vue
@@ -10,8 +10,10 @@
         <div id="map-filter">
             <here-map :driverOnRoute="driverOnRoute"></here-map>
         </div>
-        <div id="drive-button">
-            <p>{{buttonText}}</p>
+        <div id="drive-button-wrapper">
+            <div id="drive-button" @click="onButtonClick">
+                <p :style="{color: textColor}">{{buttonText}}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -29,17 +31,68 @@
                 buttonText: "",
                 driverOnRoute: false,
                 routeTimeMinutes: 0,
-                routeTimeSeconds: 0
+                routeTimeSeconds: 0,
+                routeInterval: null,
+                textColor: '#000'
             }
         },
         computed: {
             routeTime() {
-                return this.routeTimeMinutes + ":" + this.routeTimeSeconds;
+                return this.toGoodNumber(this.routeTimeMinutes) + ":" + this.toGoodNumber(this.routeTimeSeconds);
             }
         },
         methods: {
             toGoodNumber(num) {
-                
+                num = String(num);
+                if(num.length < 2) num = "0" + num;
+                return num;
+            },
+            onButtonClick() {
+                this.driverOnRoute = !this.driverOnRoute;
+
+                this.changeButtonText();
+
+                let elButton = document.getElementById('drive-button');
+                let rtp = document.getElementById('driver-clock');
+                let map = document.getElementById('map-filter');
+
+                if(this.driverOnRoute) {
+                    let contWidth = document.getElementById('drive-button-wrapper').offsetWidth;
+                    elButton.style.marginBottom = 5 + "px";
+                    elButton.style.width = contWidth - 10 + "px";
+                    elButton.style.marginLeft = 5 + "px";
+                    elButton.style.marginRight = 5 + "px";
+
+                    rtp.style.textShadow = "0 3px 6px rgba(0,0,0,0.55)";
+
+                    map.style.filter = "contrast(1)";
+
+                    this.routeTimeSeconds = 0;
+                    this.routeTimeMinutes = 0;
+
+                    this.routeInterval = setInterval(() => {
+                        this.routeTimeSeconds += 1;
+                        if(this.routeTimeSeconds > 59){
+                            this.routeTimeSeconds = 0;
+                            this.routeTimeMinutes += 1;
+                        }
+                    }, 1000);
+                } else {
+                    elButton.style.marginBottom = 115 + "px";
+                    elButton.style.width = "255px";
+                    elButton.style.marginLeft = "0";
+                    elButton.style.marginRight = "0";
+
+                    rtp.style.textShadow = "";
+
+                    map.style.filter = "contrast(0.5)";
+
+                    clearInterval(this.routeInterval);
+                }
+            },
+            changeButtonText() {
+                if(this.driverOnRoute){ this.buttonText = "Закончить движение"; this.textColor = "#B13C23"}
+                else { this.buttonText = "Начать движение"; this.textColor = "#23B164"}
             }
         },
         mounted() {
@@ -47,8 +100,10 @@
                 this.driverFullName = data.fullname;
                 this.driverRoute = data.trackNumber;
             });
+
+            this.changeButtonText();
         }
-    }/*get-user-info*/
+    }
 </script>
 
 <style>
@@ -87,11 +142,57 @@
     }
 
     #driver-clock {
+        width: 100%;
         position: absolute;
         z-index: 2;
         font-size: 4.5em;
         color: white;
+        text-align: center;
         padding-top: 70px;
         text-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+
+        transition: 0.4s ease-in-out;
+    }
+
+    #driver-clock p {
+        padding: 0;
+        margin: 0;
+    }
+
+    #map-filter {
+        filter: contrast(0.5);
+    }
+
+    #drive-button-wrapper {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #drive-button {
+        height: 55px;
+        width: 255px;
+        position: absolute;
+        bottom: 0;
+        margin-bottom: 115px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 1.5em;
+        background-color: white;
+        border-radius: 10px;
+        box-shadow: 0 2px 6px rgba(0,0,0,0.25);
+
+        transition: 0.4s ease-in-out;
+    }
+
+    #drive-button p {
+        padding: 0;
+        margin: 0;
+    }
+
+    #drive-button:hover {
+        cursor: pointer;
     }
 </style>

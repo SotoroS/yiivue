6e00a8109cb991f2f506f6b54ed4a03506953a11
pershmaj <dmitry@averin.pro>
2019-03-16 10:26:27 +0300

    vue struct

---
+++ app/components/HereMap.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="here-map">sadasd</div>
+</template>
+
+<script>
+    export default {
+        name: 'here-map',
+        mounted() {
+            navigator.geolocation.getCurrentPosition(
+                function(position) {
+                    alert('Последний раз вас засекали здесь: ' +
+                        position.coords.latitude + ", " + position.coords.longitude);
+                }
+            );
+        },
+    }
+</script>
+
+<style>
+
+</style>
--- app/pages/UserView.vue
+++ app/pages/UserView.vue
@@ -1,12 +1,17 @@
 <template>
     <div id="user-view" class="user-view">
-        User view
+        <here-map></here-map>
+        <search></search> 
     </div>
 </template>
 
 <script>
+    import HereMap from '../components/HereMap'
+    import Search from '../components/Search'
+
     export default {
         name: 'user-view',
+        components: {'here-map': HereMap, 'search': Search}
     }
 </script>
 
